649dc91c96a2ba09a231c0a8a40181231b57a85d
sweet-smart <alewidavid178@gmail.com>
2024-09-25 15:36:06 +0100

    first comment

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  //int age;
+  //cout <<"Enter your age:";
+  //cin >> age;
+  //cout << " You're " << age << " years old ";
+
+  string name;
+  cout <<"Enter your name:";
+  getline(cin,name);
+
+  cout<<"Hello"<<name;
+
+  return 0;
+}
